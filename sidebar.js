02c641a1455500269605c17064bc944002c7e06f
Hyosubin <hyosubin@gmail.com>
2022-02-04 18:09:26 +0900

    fininshed this examples

--- sidebar.js
+++ sidebar.js
@@ -1,12 +1,17 @@
 const body = document.querySelector('body');
       sidebar = document.querySelector('.sidebar');
-      toggle = document.querySelector('.toggle');
+      toggle = document.querySelector('.toggle'); //<i class='bx bx-chevron-right toggle'></i>
       searchBtn = document.querySelector('.search-box');
       modeSwitch = document.querySelector('.toggle-switch');
       modeText = document.querySelector('.mode-text');
 
 toggle.addEventListener('click', () => {
   sidebar.classList.toggle('close');
+});
+
+
+searchBtn.addEventListener('click', () => {
+  sidebar.classList.remove('close');
 });
 
 modeSwitch.addEventListener('click', () => {
